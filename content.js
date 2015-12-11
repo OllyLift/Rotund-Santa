1b7c5e388b18a7aa32b21958540a7368447fe370
Colin Hayes <colin.hayes@aptify.com>
2015-12-11 12:38:09 -0500

    fancified plus more diggs.

--- content.js
+++ content.js
@@ -1,5 +1,14 @@
 var elements = document.getElementsByTagName('*');
-
+var buzzWords = [
+    {"key":"issue", "val":"mystical beast"},
+    {"key":"clear your cache", "val":"PRAY TO THE GODS"},
+    {"key":"pivot grid", "val":"fancy paper weight"},
+    {"key":"R&D", "val":"bird people"},
+    {"key":"5.5.4","val":"digital messiah"},
+    {"key":"AIP","val":"popularity contest"},
+    {"key":"KPI","val":"worth to the company"},
+    {"key":"hours entry app","val":"root canel"}
+];
 for (var i = 0; i < elements.length; i++) {
     var element = elements[i];
 
@@ -7,44 +16,15 @@ for (var i = 0; i < elements.length; i++) {
         var node = element.childNodes[j];
 
         if (node.nodeType === 3) {
-            var text = node.nodeValue;
-            var replacedText = text.replace(/issue/gi, "mystical beast");
+            for(var h=0;h<buzzWords.length;h++){
+                var text = node.nodeValue;
+                var replacedText = text.replace(buzzWords[h].key, buzzWords[h].val);
 
-            if (replacedText !== text) {
-                element.replaceChild(document.createTextNode(replacedText), node);
+                if (replacedText !== text) {
+                    element.replaceChild(document.createTextNode(replacedText), node);
+                }
             }
-        }
-		        if (node.nodeType === 3) {
-            var text = node.nodeValue;
-            var replacedText = text.replace(/clear your cache/gi, "PRAY TO THE GODS");
 
-            if (replacedText !== text) {
-                element.replaceChild(document.createTextNode(replacedText), node);
-            }
-        }
-       if (node.nodeType === 3) {
-            var text = node.nodeValue;
-            var replacedText = text.replace(/pivot grid/gi, "fancy paper weight");
-
-            if (replacedText !== text) {
-                element.replaceChild(document.createTextNode(replacedText), node);
-            }
-        }
-		if (node.nodeType === 3) {
-            var text = node.nodeValue;
-            var replacedText = text.replace(/R&D/gi, "bird people");
-
-            if (replacedText !== text) {
-                element.replaceChild(document.createTextNode(replacedText), node);
-            }
-        }
-		if (node.nodeType === 3) {
-            var text = node.nodeValue;
-            var replacedText = text.replace(/5.5.4/gi, "digital messiah");
-
-            if (replacedText !== text) {
-                element.replaceChild(document.createTextNode(replacedText), node);
-            }
         }
     }
 }

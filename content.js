dc856b8797667281d22a9b8f10ea01063a9bc9d2
roadwarrior99 <colinmhayes@gmail.com>
2017-02-16 18:27:33 -0600

    updates

--- content.js
+++ content.js
@@ -1,21 +1,13 @@
 var elements = document.getElementsByTagName('*');
 var buzzWords = [
-    {"key":/issue/gi, "val":"mystical beast"},
-    {"key":/clear your cache/gi, "val":"PRAY TO THE GODS"},
-    {"key":/pivot grid/gi, "val":"fancy paper weight"},
-    {"key":/R&D/gi, "val":"bird people"},
-    {"key":/5.5.4/gi,"val":"digital messiah"},
-    {"key":/AIP/gi,"val":"popularity contest"},
-    {"key":/KPI/gi,"val":"Key Performance Indicator"},
-    {"key":/hours entry app/gi,"val":"root canal"},
-    {"key":/DITO/gi,"val":"colonoscopy"},
-    {"key":/allocation/gi,"val":"slave auction"},
-    {"key":/HBG/gi,"val":"Hours Billing Group"},
-    {"key":/Wendy Salas/gi,"val":"Wendingo"},
-    {"key":/feedback/gi,"val":"holla back"},
-    {"key":/Aaron James/gi,"val":"Stairway to Meatloaf"},
-    {"key":/Joshua Hiller/gi,"val":"Wixxerd"},
-    {"key":/Colin Hayes/gi,"val":"Seyah Niloc"}
+    { "key": /issue/gi, "val": "mystical beast" },
+    { "key": /clear your cache/gi, "val": "PRAY TO THE GODS" },
+    { "key": /alt-right/gi, "val": "Nazi" },
+    { "key": /donald trump/gi, "val": "Cheeto Faced Ass Clown" },
+    { "key": /milo yiannopoulos/gi, "val": "Neo-Nazi Clown" },
+    { "key": /betsy devos/gi, "val": "Death of Public Education" },
+    { "key": /fake news/gi, "val": "news" },
+    { "key": /fox news/gi, "val": "right-wing opinions" }
 ];
 for (var i = 0; i < elements.length; i++) {
     var element = elements[i];
@@ -24,7 +16,7 @@ for (var i = 0; i < elements.length; i++) {
         var node = element.childNodes[j];
 
         if (node.nodeType === 3) {
-            for(var h=0;h<buzzWords.length;h++){
+            for (var h = 0; h < buzzWords.length; h++) {
                 var text = node.nodeValue;
                 var replacedText = text.replace(buzzWords[h].key, buzzWords[h].val);
 
